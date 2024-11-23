b833ffc8f3379840eb4c81bfd84131e895a95ec9
angelvargass <59036972+angelvargass@users.noreply.github.com>
2024-11-22 22:43:31 -0600

    fix association

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,11 +55,15 @@ resource "aws_internet_gateway" "igw" {
 }
 
 #ROUTE TABLES AND IGW ASSOCIATION
+resource "aws_route_table_association" "public_subnet_igw_association" {
+  route_table_id = aws_route_table.public_rt.id
+  gateway_id     = aws_internet_gateway.igw.id
+}
+
 resource "aws_route_table_association" "public_subnet_association" {
   count          = length(var.public_subnets_cidr)
   route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.public_subnets[count.index].id
-  gateway_id     = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
