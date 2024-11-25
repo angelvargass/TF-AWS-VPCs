b9dee89c983608320854a218bc6b3cb355bc0642
angelvargass <59036972+angelvargass@users.noreply.github.com>
2024-11-25 16:47:01 -0600

    fixed sg rules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,13 +18,16 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_ingress_rule" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
+  to_port = 22
   ip_protocol = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "ssh_egress_rule" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "tcp"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
 }
 
 #SUBNETS CREATION
